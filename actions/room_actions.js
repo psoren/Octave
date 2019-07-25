95a9bd4c06e6f16795b70bc95f814a7581195024
Parker <p.sorenson@me.com>
2019-07-25 17:23:14 -0600

    removed unnecessary code

--- actions/room_actions.js
+++ actions/room_actions.js
@@ -1,12 +1,4 @@
-import * as firebase from 'firebase';
-import 'firebase/firestore';
-
-import {
-  UPDATE_ROOM,
-  TOGGLE_PLAYBACK,
-  NEXT_SONG,
-  PREVIOUS_SONG
-} from './types';
+import { UPDATE_ROOM } from './types';
 
 export const updateRoom = data => ({
   type: UPDATE_ROOM,
